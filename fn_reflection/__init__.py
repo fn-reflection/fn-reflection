58a71a293fb7ff0990eebc1edbd21495a63804c4
Naoki Fujita <fujitanaoki0718@gmail.com>
2019-02-17 22:53:13 +0900

    make import module invisible

--- fn_reflection/__init__.py
+++ fn_reflection/__init__.py
@@ -1,2 +1,2 @@
 from . import util
-version = '0.2.1'
+version = '0.2.2'
--- fn_reflection/util.py
+++ fn_reflection/util.py
@@ -1,10 +1,10 @@
 # pylint: disable=missing-docstring,invalid-name
 # %%
-import typing
-import collections
+__all__ = ['run_once', 'chain']
+import fn_reflection._capcell as _capcell
 
 
-def run_once(f: typing.Callable):
+def run_once(f: _capcell.typing.Callable):
     def wrapper(*args, **kwargs):
         if not wrapper.has_run:
             wrapper.has_run = True
@@ -13,16 +13,16 @@ def run_once(f: typing.Callable):
     return wrapper
 
 
-def chain(value, fargs: typing.Iterable):
+def chain(value, fargs: _capcell.typing.Iterable):
     for farg in fargs:
-        if not isinstance(farg, collections.abc.Iterable):
+        if not isinstance(farg, _capcell.collections.abc.Iterable):
             f = farg
             val = f(value)
         else:
             f, arg = farg
             if isinstance(arg, dict):
                 val = f(value, **arg)
-            elif isinstance(arg, collections.abc.Iterable):
+            elif isinstance(arg, _capcell.collections.abc.Iterable):
                 val = f(value, *arg)
             else:
                 val = f(value, arg)
