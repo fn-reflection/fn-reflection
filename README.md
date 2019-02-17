6dab7175795fed071fd9d34c83be46d8cb7e1c43
Naoki Fujita <fujitanaoki0718@gmail.com>
2019-02-17 21:38:13 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fn-reflection
-public repository of python utility scripts
+python utility scripts for my private projects
+
+## How to install
+```
+$ pip install fn-reflection
+```
+or
+
+## How to use
+```
+import fn-reflection
+```
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup
+
+if sys.version_info[:2] < (3, 5):
+    raise RuntimeError("Python version >= 3.5 required.")
+
+
+def main():
+    setup()
+
+
+if __name__ == '__main__':
+    main()
