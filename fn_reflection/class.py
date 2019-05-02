781743e3246cf45ecfd9ca0e9d94e51a5a257398
Naoki Fujita <fujitanaoki0718@gmail.com>
2019-05-02 22:08:01 +0900

    add SimpleCounter class

---
+++ fn_reflection/class.py
@@ -0,0 +1,16 @@
+class SimpleCounter:
+    def __init__(self, initial: int = 0):
+        self.c = initial
+
+    def inc(self):
+        self.c += 1
+
+    def dec(self):
+        self.c -= 1
+
+    @property
+    def count(self):
+        return self.c
+
+    def set(self, value: int):
+        self.c = value
