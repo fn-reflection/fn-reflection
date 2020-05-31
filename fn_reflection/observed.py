e392410dffdb8f876f1bd0c8fad426e370f80be1
Naoki Fujita <fujitanaoki0718@gmail.com>
2020-05-31 23:45:38 +0900

    rename instance variable properly

--- fn_reflection/observed.py
+++ fn_reflection/observed.py
@@ -4,7 +4,7 @@ from typing import List, Callable, Any
 class Observed:
     def __init__(self, value: Any = None, callbacks: List[Callable[[Any], Any]] = None):
         self._v: Any = value
-        self._cbs: List[Callable[[Any], Any]] = callbacks if callbacks else []
+        self._callbacks: List[Callable[[Any], Any]] = callbacks if callbacks else []
 
     @property
     def v(self):
@@ -13,13 +13,13 @@ class Observed:
     @v.setter
     def v(self, value: Any):
         self._v = value
-        for c in self._cbs:
+        for c in self._callbacks:
             c(self._v)
 
     @property
-    def cbs(self):
-        return self._cbs
+    def callbacks(self):
+        return self._callbacks
 
-    @cbs.setter
-    def cbs(self, callbacks):
-        self._cbs = callbacks
+    @callbacks.setter
+    def callbacks(self, callbacks):
+        self._callbacks = callbacks
