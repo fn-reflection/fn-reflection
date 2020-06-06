926bff546e2597983dd0efd1382b48547931822a
Naoki Fujita <fujitanaoki0718@gmail.com>
2020-06-06 20:30:18 +0900

    add some useful object for threading and signaling

--- fn_reflection/threading.py
+++ fn_reflection/threading.py
@@ -17,27 +17,35 @@ class WithLock:
             return super().__getattribute__(name)
 
 
-class ObservedWithLock(WithLock):
+class ObservedWithLock:
     def __init__(self, data: Any = None, callbacks: List[Callable[[Any], Any]] = None):
-        super().__init__()
-        cbs = callbacks if callbacks else []
+        super().__setattr__('_lock', Lock())
         self._data: Any = data
-        self._callbacks: List[Callable[[Any], Any]] = cbs
+        self._callbacks: List[Callable[[Any], Any]
+                              ] = callbacks if callbacks else []
+
+    def __setattr__(self, name, data):
+        with super().__getattribute__('_lock'):
+            super().__setattr__(name, data)
+
+    def __getattribute__(self, name):
+        with super().__getattribute__('_lock'):
+            return super().__getattribute__(name)
 
     @property
     def data(self):
-        return self._data
+        return super().__getattribute__('_data')
 
     @data.setter
     def data(self, value: Any):
-        self._data = value
-        for c in self._callbacks:
-            c(self._data)
+        super().__setattr__('_data', value)
+        for c in super().__getattribute__('_callbacks'):
+            c(super().__getattribute__('_data'))
 
     @property
     def callbacks(self):
-        return self._callbacks
+        return super().__getattribute__('_callbacks')
 
     @callbacks.setter
     def callbacks(self, callbacks):
-        self._callbacks = callbacks
+        super().__setattr__('_callbacks', callbacks)
