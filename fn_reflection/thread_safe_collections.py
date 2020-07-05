b5bc89cb3567d62878950754b727423d4ce11b45
Naoki Fujita <fujitanaoki0718@gmail.com>
2020-07-06 00:30:39 +0900

    refact: MutableMapping Interfaceを実装する方針に変更する

--- fn_reflection/thread_safe_collections.py
+++ fn_reflection/thread_safe_collections.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, deque
+from collections.abc import MutableMapping
 from threading import Lock
 
 
@@ -35,30 +36,108 @@ class ObservedValue:
         super().__setattr__('_value_setter_callbacks', value_setter_callbacks)
 
 
-class ObservedDict(dict):
+class ObservedDict(MutableMapping):
     def __init__(self, *args, **kwargs):
+        self.store = dict()
         self._lock = Lock()
         self.setitem_callbacks = []
-        super().__init__(*args, **kwargs)
+        self.update_callbacks = []
+        self.store.update(dict(*args, **kwargs))
+
+    def __getitem__(self, key):
+        with self._lock:
+            return self.store[key]
+
+    def get(self, key, default=None):
+        with self._lock:
+            return self.store.get(key, default)
 
     def __setitem__(self, key, value):
         with self._lock:
-            super().__setitem__(key, value)
-        for callback in self.setitem_callbacks:
-            callback(self, key, value)
+            self.store[key] = value
+            for callback in self.setitem_callbacks:
+                callback(self.store, key, value)
+
+    def __delitem__(self, key):
+        with self._lock:
+            del self.store[key]
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        with self._lock:
+            return len(self.store)
+
+    def __repr__(self):
+        return repr(self.store)
+
+    def __str__(self):
+        return str(self.store)
+
+    def update(self, m=None, **kwargs):
+        with self._lock:
+            if m is not None:
+                self.store.update(m, **kwargs)
+            else:
+                self.store.update(**kwargs)
+            for callback in self.update_callbacks:
+                callback(self.store, m, kwargs)
 
 
-class ObservedDefaultDict(defaultdict):
-    def __init__(self, default_factory, *args, **kwargs):
+class ObservedDefaultDict(MutableMapping):
+    def __init__(self, default_factory, **kwargs):
+        self.store = dict()
         self._lock = Lock()
+        self.default_factory = default_factory
         self.setitem_callbacks = []
-        super().__init__(default_factory, *args, **kwargs)
+        self.update_callbacks = []
+        self.store.update(**kwargs)
+
+    def __getitem__(self, key):
+        with self._lock:
+            value = self.store.get(key)
+            if value:
+                return value
+            default = self.default_factory()
+            self.store[key] = default
+            return default
+
+    def get(self, key, default=None):
+        with self._lock:
+            return self.store.get(key, default)
 
     def __setitem__(self, key, value):
         with self._lock:
-            super().__setitem__(key, value)
-        for callback in self.setitem_callbacks:
-            callback(self, key, value)
+            self.store[key] = value
+            for callback in self.setitem_callbacks:
+                callback(self.store, key, value)
+
+    def __delitem__(self, key):
+        with self._lock:
+            del self.store[key]
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        with self._lock:
+            return len(self.store)
+
+    def __repr__(self):
+        return repr(self.store)
+
+    def __str__(self):
+        return str(self.store)
+
+    def update(self, m=None, **kwargs):
+        with self._lock:
+            if m is not None:
+                self.store.update(m, **kwargs)
+            else:
+                self.store.update(**kwargs)
+            for callback in self.update_callbacks:
+                callback(self.store, m, kwargs)
 
 
 class ObservedDeque(deque):
