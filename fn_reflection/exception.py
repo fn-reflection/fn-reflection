18b21a57bcf5848bb1ab9538452a84f53c4391a2
NaokiFujita <fujitanaoki0718@gmail.com>
2020-10-01 03:33:20 +0900

    feat: rename and refine

--- fn_reflection/exception.py
+++ fn_reflection/exception.py
@@ -3,10 +3,9 @@ import sys
 from typing import Callable
 import pickle
 import traceback
-from tblib import pickling_support
-from .pickle import to_pickle_with_timestamp
-pickling_support.install()
+from .pickle import to_pickle_with_timestamp, update_cloudpickler_dispatchtable
 
+update_cloudpickler_dispatchtable()
 
 def try_with_dump_traceback(procedure: Callable, logger, file_prefix: str = '',
                             pickle_protocol: int = pickle.HIGHEST_PROTOCOL, makedirs=True):
@@ -20,27 +19,29 @@ def try_with_dump_traceback(procedure: Callable, logger, file_prefix: str = '',
             obj=tb, prefix=file_prefix, makedirs=makedirs, protocol=pickle_protocol)
 
 
-def try_with_post_mortem(procedure: Callable, callback: Callable, *args, **kwargs):
+def try_with_excepthook(procedure: Callable, excepthook: Callable, *args, **kwargs):
     try:
         res = procedure(*args, **kwargs)
         return res
     except Exception as _e:
         _, _, tb = sys.exc_info()
-        callback(tb)
+        excepthook(tb)
 
 
-def locals_json(tb, max_frame=100):
+def locals_json(tb, skip_frame=0, max_frame=100):
     local_var_list = []
-    for _ in range(max_frame):
-        local_vars = dict(tb.tb_frame.f_locals.items())
-        local_var_list.append(local_vars)
+    for _ in range(skip_frame):
         tb = tb.tb_next
         if tb is None:
-            break
-    return local_var_list
+            return reversed(local_var_list)
+    for _ in range(max_frame-skip_frame):
+        local_var_list.append(tb.tb_frame.f_locals)
+        tb = tb.tb_next
+        if tb is None:
+            return reversed(local_var_list)
 
 
-def summary_message(tb, max_frame=10):
-    stack_list = traceback.format_tb(tb)[:max_frame]
+def summary_message(tb, skip_frame=0, max_frame=10):
+    stack_list = reversed(traceback.format_tb(tb)[skip_frame:max_frame])
     local_var_list = [str(local_vars) for local_vars in locals_json(tb, max_frame=max_frame)]
     return "\n".join(["\n".join(pair) for pair in zip(local_var_list, stack_list)])
