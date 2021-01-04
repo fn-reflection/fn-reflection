1e2c1c812dd9d119f3e2b533a8bc5988a1f656d3
Naoki Fujita <fujitanaoki0718@gmail.com>
2021-01-05 00:47:45 +0900

    feat: append modeにする

--- fn_reflection/file.py
+++ fn_reflection/file.py
@@ -4,9 +4,8 @@ from pathlib import Path
 
 
 def to_file(obj, filepath: str):
-    with open(filepath, mode='wb') as f:
+    with open(filepath, mode='a') as f:
         f.write(obj)
-
 
 def to_file_with_timestamp(obj, prefix: str = '', makedirs: bool = True):
     filepath = Path(f'{prefix}{int(time.time()*1000)}')
